f3d1e0b3ff0be387ae0442c811b6fd1778a371f7
Ira Krakow <ira.krakow@gmail.com>
2022-12-27 18:21:26 -0500

    More  edits

--- python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/1_extract_CSV_file_from_array.py
+++
@@ -1,1 +0,0 @@
-# Extract data from the given SalaryGender CSV file and store the data from each column in a separate NumPy array
---
+++ python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/1_extract_CSV_file_from_array.py
@@ -0,0 +1,1 @@
+# Extract from the given SalaryGender CSV file and store the data from each #column in a separate NumPy array
