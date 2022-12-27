a42e6b5ad9fe7e2665dedb27dbe4f07bfb10820e
Ira Krakow <ira.krakow@gmail.com>
2022-12-27 18:29:43 -0500

    Adding more files

---
+++ python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/6_create_numpy_arrays.py
@@ -0,0 +1,1 @@
+# Create a numpy array [[0, 1, 2], [ 3, 4, 5], [ 6, 7, 8],[ 9, 10, 11]]) and filter the elements greater than 5.
---
+++ python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/7_create_NaN.py
@@ -0,0 +1,3 @@
+# Create a numpy array having NaN (Not a Number) and print it.
+# array([ nan, 1., 2., nan, 3., 4., 5.])
+# Print the same array omitting all elements which are nan
