b82e12b2240850d97e130c32876e40c620369fab
Ira Krakow <ira.krakow@gmail.com>
2022-12-30 19:28:11 -0500

    problem 5 count values in array

--- python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/5_count_values_in_array.py
+++ python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/5_count_values_in_array.py
@@ -1,4 +1,26 @@
-# How do you Count The Number Of Times Each Value Appears In An Array Of Integers?
-# Example:
-# [0, 5, 4, 0, 4, 4, 3, 0, 0, 5, 2, 1, 1, 9]
-# Answer should be array([4, 2, 1, 1, 3, 2, 0, 0, 0, 1]) which means 0 comes 4 times, 1 comes 2 times, 2 comes 1 time, 3 comes 1 time and so on.
+def count_values(arr):
+    # Create a dictionary with default values of 0
+    count = {}
+
+    # Iterate through the array and count the number of occurrences of each value
+    for value in arr:
+        if value in count:
+            count[value] += 1
+        else:
+            count[value] = 1
+
+    # Print the number and the number of occurrences for each value, sorted by the key (number)
+    for value in sorted(count):
+        print(f"{value}: {count[value]}")
+
+
+# Test the function
+count_values([0, 5, 4, 0, 4, 4, 3, 0, 0, 5, 2, 1, 1, 9])
+# Should print:
+# 0: 4
+# 1: 2
+# 2: 1
+# 3: 1
+# 4: 3
+# 5: 2
+# 9: 1
