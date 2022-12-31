1ff16454e2f49c58be8f879078a6f194bfd34f32
Ira Krakow <ira.krakow@gmail.com>
2022-12-30 20:06:11 -0500

    problem 9

--- python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/9_random_mean.py
+++ python-tutorials/Edureka/4_Introduction_to_Numpy_and_Pandas/Case_Study_1/9_random_mean.py
@@ -1,1 +1,14 @@
-# Create a random vector of size 30 and find the mean value.
+import numpy as np
+
+# Create a random vector of size 30 with integers between 0 and 100
+vector = np.random.randint(0, 101, 30)
+
+# Round each number in the vector to 3 decimal places
+vector = [round(num, 3) for num in vector]
+
+# Find the mean value of the vector
+mean = np.mean(vector)
+
+# Print the vector and the mean value
+print(f'Vector: [{", ".join(map(str, vector))}]')
+print(f'Mean value: {mean:.3f}')
