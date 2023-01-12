edc6e19b2016db37ffb6ed28c7cc41835383b525
Ira Krakow <ira.krakow@gmail.com>
2023-01-11 23:44:43 -0500

    Read first 5 rows of .xlsx and .csv files

--- python-tutorials/YouTube-Tutorials/pandas_tutorial/pandas_play.py
+++ python-tutorials/YouTube-Tutorials/pandas_tutorial/pandas_play.py
@@ -1,3 +1,12 @@
 import pandas as pd
-df = pd.read_csv('pokemon.csv')
+
+# read pokemon_data csv file
+print('reading first 5 rows of .csv file')
+df = pd.read_csv('pokemon_data.csv')
 print(df.head(5))
+
+print()
+print('---------------------------------------------------------------')
+print('reading first 5 rows of excel file')
+df_xlsx = pd.read_excel('pokemon_data.xlsx')
+print(df_xlsx.head(5))
