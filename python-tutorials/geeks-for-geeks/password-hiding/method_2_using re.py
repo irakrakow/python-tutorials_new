f0a5537023430c9570e94f8bebf8f03f6069a0c1
Ira Krakow <ira.krakow@gmail.com>
2023-01-13 09:33:06 -0500

    re.py

--- python-tutorials/geeks-for-geeks/password-hiding/method_2_using re.py
+++ python-tutorials/geeks-for-geeks/password-hiding/method_2_using re.py
@@ -1,22 +1,24 @@
-# importing re library
+# importing re library (regular expressions)
 import re
 
-def main():
-	passwd = 'Geek12@'
-	reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
-	
-	# compiling regex
-	pattern = re.compile(reg)
-	
-	# searching regex				
-	mat = re.search(pattern, passwd)
-	
-	# validating conditions
-	if mat:
-		print("Password is valid.")
-	else:
-		print("Password invalid !!")
 
-# Driver Code	
+def main():
+    #passwd = 'Geek12@'
+    passwd = input("Password: ")
+    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+
+    # compiling regex
+    pattern = re.compile(reg)
+    # searching regex
+    match = re.search(pattern, passwd)
+
+    # validating conditions
+    if match:
+        print("Password is valid.")
+    else:
+        print("Password invalid !!")
+
+
+# Driver Code
 if __name__ == '__main__':
-	main()
+    main()
